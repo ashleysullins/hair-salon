01cd128bddac7460cbd873c3c8857b6e0ab1e690
Ashley Sullins <ashley.sullins@gmail.com>
2015-08-28 11:59:13 -0700

    Add initial Spark layouts

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,21 +12,51 @@ public class App {
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String,Object>();
       model.put("template", "templates/home.vtl");
+      
+      // String sName = request.queryParams("stylistName");
+      // Stylist newStylist = new Stylist(sname);
+      
+      String name = request.queryParams("clientName");
+      int phone = Integer.parseInt(request.queryParams("clientPhone"));
+      int stylist_id = Integer.parseInt(request.queryParams("stylistid"));
+      Client newClient = new Client(name, phone, stylist_id);
+      newClient.save();
+      
+      model.put("clients", Client.all());
+      model.put("stylists", Stylist.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    
+    post("/new-client", (request, response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      model.put("template", "templates/new-client.vtl");
+      model.put("clients", Client.all());
+      model.put("stylists", Stylist.all());
 
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-
-    get("/results", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/results.vtl");
-
-      //Variables that you'd like to call on each page go here
+    
+    post("/new-stylist", (request, response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      model.put("template", "templates/new-stylist.vtl");
+      model.put("clients", Client.all());
+      model.put("stylists", Stylist.all());
 
       return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    
+    get("/edit-client", (request, response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      model.put("template", "templates/edit-client.vtl");
 
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    
+    post("/edit-client-success", (request, response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      model.put("template", "templates/edit-client-success.vtl");
 
+      return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
-
-  //Algorithm goes here
 }
