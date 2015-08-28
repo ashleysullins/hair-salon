2a9a5c62beb521fb4a80b5461f33c957b96a9bd8
Ashley Sullins <ashley.sullins@gmail.com>
2015-08-28 08:37:35 -0700

    Add postgres info to template

--- src/test/java/AppUnitTest.java
+++ src/test/java/AppUnitTest.java
@@ -2,6 +2,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class AppUnitTest {
+  
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
   @Test
   public void methodName_testDescription_expectedOutcome() {
